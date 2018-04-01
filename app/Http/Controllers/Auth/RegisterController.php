dfc62fbf5ac26607c236f64ff4718a5b3c139341
Youghourta Benali <youghourta@gmail.com>
2018-04-01 18:33:17 +0100

    task01: generate an API key for the user when she registers

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'api_key' => str_random(60)
         ]);
     }
 }
